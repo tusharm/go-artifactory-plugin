d02a6c46f1d1a2a5718d8f69b937fa5d77faf615
TusharM <tushar.madhukar@gmail.com>
2014-06-01 21:11:00 +0530

    Added compile and test dependencies; setting up a basic scaffolding

---
+++ src/main/java/com/tw/go/plugins/artifactory/Main.java
@@ -0,0 +1,6 @@
+package com.tw.go.plugins.artifactory;
+
+public class Main {
+  public static void main(String[] args) {
+  }
+}
---
+++ src/test/java/com/tw/go/plugins/artifactory/SomeTest.java
@@ -0,0 +1,13 @@
+package com.tw.go.plugins.artifactory;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SomeTest {
+    @Test
+    public void test() {
+        assertThat(true, is(true));
+    }
+}
