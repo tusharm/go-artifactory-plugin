3fc44858766204557a0a1d7e8dbfd9731c698724
TusharM <tushar.madhukar@gmail.com>
2014-06-18 13:32:11 +0530

    Task config now takes relative path to artifact to be uploaded

--- src/main/java/com/tw/go/plugins/artifactory/task/PublishTaskExecutor.java
+++ src/main/java/com/tw/go/plugins/artifactory/task/PublishTaskExecutor.java
@@ -6,12 +6,15 @@ import com.thoughtworks.go.plugin.api.task.TaskExecutionContext;
 import com.thoughtworks.go.plugin.api.task.TaskExecutor;
 import com.tw.go.plugins.artifactory.task.config.PublishTaskConfig;
 
+import static com.tw.go.plugins.artifactory.task.config.PublishTaskConfig.PATH;
 import static com.tw.go.plugins.artifactory.task.config.PublishTaskConfig.URL;
 
 public class PublishTaskExecutor implements TaskExecutor {
     @Override
     public ExecutionResult execute(TaskConfig config, TaskExecutionContext context) {
         context.console().printLine("Artifactory URL : " + config.getValue(URL));
+        context.console().printLine("Path : " + config.getValue(PATH));
+
         return ExecutionResult.success("Finished running Artifactory plugin!");
     }
 }
--- src/main/java/com/tw/go/plugins/artifactory/task/config/PublishTaskConfig.java
+++ src/main/java/com/tw/go/plugins/artifactory/task/config/PublishTaskConfig.java
@@ -4,12 +4,18 @@ import com.thoughtworks.go.plugin.api.response.validation.ValidationError;
 import com.thoughtworks.go.plugin.api.response.validation.ValidationResult;
 import com.thoughtworks.go.plugin.api.task.TaskConfig;
 
+import java.io.File;
+
+import static java.lang.String.format;
+
 public class PublishTaskConfig {
     public static final String URL = "url";
+    public static final String PATH = "path";
 
     public TaskConfig get() {
         TaskConfig taskConfig = new TaskConfig();
         taskConfig.addProperty(URL);
+        taskConfig.addProperty(PATH);
         return taskConfig;
     }
 
@@ -19,6 +25,11 @@ public class PublishTaskConfig {
         if (!taskConfig.getValue(URL).matches("http(s)?://.*")) {
             result.addError(new ValidationError(URL, "Invalid HTTP URL"));
         }
+
+        if (taskConfig.getValue(PATH).startsWith(File.separator)) {
+            result.addError(new ValidationError(PATH, format("Relative path shouldn't start with %s", File.separator)));
+        }
+
         return result;
     }
 }
