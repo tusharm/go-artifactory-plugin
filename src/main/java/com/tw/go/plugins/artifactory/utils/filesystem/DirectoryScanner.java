6f6d6581c9c5a11a8202d20f4d67cf3fd2c6c9a3
TusharM <tushar.madhukar@gmail.com>
2014-08-07 12:54:56 +0530

    Fixing path issues on Windows so that DirectoryScanner is able to find files by pattern

--- src/main/java/com/tw/go/plugins/artifactory/utils/filesystem/DirectoryScanner.java
+++ src/main/java/com/tw/go/plugins/artifactory/utils/filesystem/DirectoryScanner.java
@@ -6,13 +6,13 @@ import com.tw.go.plugins.artifactory.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.Collection;
-import java.util.EnumSet;
+import java.util.*;
 
 import static com.google.common.collect.Collections2.transform;
 import static java.io.File.separator;
+import static java.lang.Integer.MAX_VALUE;
 import static java.lang.String.format;
-import static java.nio.file.FileVisitOption.FOLLOW_LINKS;
+import static java.util.Collections.EMPTY_SET;
 
 public class DirectoryScanner {
     private Logger logger = Logger.getLogger(this.getClass());
@@ -27,22 +27,27 @@ public class DirectoryScanner {
 
     public Collection<File> scan(String pattern) {
         try {
-            String globPattern = format("glob:%s%s%s", root, separator, pattern);
+            String globPattern = format("glob:%s/%s", root, pattern).replace(separator, "/");
 
             PathMatchingVisitor visitor = new PathMatchingVisitor(fileSystem.getPathMatcher(globPattern));
-            Files.walkFileTree(root, EnumSet.of(FOLLOW_LINKS), Integer.MAX_VALUE, visitor);
-
-            logger.debug("Scanned files:");
-            return transform(visitor.matched(), new Function<Path, File>() {
-                @Override
-                public File apply(Path path) {
-                    logger.debug(path.toString());
-                    return path.toFile();
-                }
-            });
+            Files.walkFileTree(root, EMPTY_SET, MAX_VALUE, visitor);
+            return matchingFilesFrom(visitor);
         }
         catch(IOException iox) {
             throw new RuntimeException(iox);
         }
+    }
+
+
+    private Collection<File> matchingFilesFrom(PathMatchingVisitor visitor) {
+        logger.debug("Scanned files:");
+
+        return transform(visitor.matched(), new Function<Path, File>() {
+            @Override
+            public File apply(Path path) {
+                logger.debug(path.toString());
+                return path.toFile();
+            }
+        });
     }
 }
