18fab8ee2874fbc9d3989b547426762af0f23f3a
Tushar Madhukar <tusharm@thoughtworks.com>
2014-08-07 15:56:30 +0530

    fix: Windows paths do not allow '*'

--- src/main/java/com/tw/go/plugins/artifactory/task/config/PathConfigElement.java
+++ src/main/java/com/tw/go/plugins/artifactory/task/config/PathConfigElement.java
@@ -2,6 +2,7 @@ package com.tw.go.plugins.artifactory.task.config;
 
 import com.thoughtworks.go.plugin.api.task.TaskConfig;
 
+import java.io.File;
 import java.nio.file.Paths;
 
 public class PathConfigElement extends ConfigElement<String> {
@@ -11,14 +12,15 @@ public class PathConfigElement extends ConfigElement<String> {
 
     @Override
     protected boolean isValid(String value) {
-        if (value.isEmpty())
-            return false;
-
-        return !Paths.get(value).isAbsolute();
+        return (value.isEmpty() || isAbsolutePath(value)) ? false : true;
     }
 
     @Override
     public String from(TaskConfig taskConfig) {
         return taskConfig.getValue(name());
+    }
+
+    private boolean isAbsolutePath(String pathString) {
+        return pathString.startsWith(File.separator) || pathString.matches("\\w:.*");
     }
 }
